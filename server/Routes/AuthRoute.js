ce3ee73e02cf81c8aa7d9b211a51c4358da7a767
Devansh Sudhanshu Bhatt <devansh@canarymail.io>
2023-05-06 16:24:42 +0530

    comments

--- server/Routes/AuthRoute.js
+++ server/Routes/AuthRoute.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { loginUser, registerUser } from '../Controllers/AuthController.js';
 
 const router = express.Router();
-// Resonspe of Server | Pipeline for Register User
+// Response of Server | Pipeline for Register User
 router.post('/register', registerUser)
 router.post('/login', loginUser)
 
